09dab01a038a4cd24c071499af4bd5fdf94ff82c
lsc1388 <scliao61413@gmail.com>
2025-03-08 10:08:58 +0800

    class1

--- class1/class1.py
+++ class1/class1.py
@@ -1,1 +1,21 @@
+# 這是單行的註解
+# ctrl + ? 可以切換滑鼠的框選範圍變成註解
+"""
+這是多行註解1
+這是多行註解2
+這是多行註解3
+"""
 
+print("hello from prji.py")  # 這是把字串印出來的指令
+
+# 基本型態 basic types
+print(1)  # 整數int
+print(1.1)  # 福點數float,只要有小數點就是浮點數
+print("hello")  # 字串str,  用雙引號:""或單引號''包起來的都是字串
+print(True)  # 布林值bool1,第一個字母要大寫
+print(None)  # 空值None
+
+# 變數variable
+a = 1  # 把這個整數存到電腦的記憶體,並且取名為A
+# =的功能是將右邊的資料存到左邊的變數空間當中
+print(a)  # 把a的店數的值印出來
