44fb6a281009284092fa436675f7f96faa29d242
lsc1388 <scliao61413@gmail.com>
2025-03-08 12:03:42 +0800

    class2

--- class2/class2.py
+++ class2/class2.py
@@ -48,3 +48,33 @@ print("------------")
 
 print(bool(123))  # True
 print(bool(0))  # True
+print(bool(-1))  # True
+print(bool(""))  # True
+print(bool("0"))  # True
+
+# input()讓使用者在終端機輸入資料
+# input() 的括弧內可以放入"提示字串"
+a = input("請輸入數字:")
+# 透過 input() 輸入的透過 input() 輸入的資料都是字串
+print(a + "1")  # 字串相加
+Print(int(a) + 1)  # 字串轉換成整數後相加
+
+# 運算子
+print(1 + 1)  # 加法
+print(1 - 1)  # 減法
+print(1 * 1)  # 乘法
+print(1 / 1)  # 除法
+print(1 % 1)  # 取餘數
+print(1 // 1)  # 取商
+print(1**1)  # 次方
+
+# 優先順序
+# 1.()括號
+# 2.**次方
+# 3.*/ // %  乘  除  取商  取餘數
+# 4.+ -  加  減
+# 正方形面積計算
+r = input("請輸入正方形的邊長:")
+r = int(r)  # 將字串轉換成整數
+area = r * r  # 計算面積
+print(f"正方形的面積是{area}")  # 印出面積
