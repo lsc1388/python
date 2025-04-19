379c78f144e986224b0d9fcd714020cecc925f3b
lsc1388 <scliao61413@gmail.com>
2025-04-19 11:09:47 +0800

    hw5.py

--- class5/hw6.py
+++ class5/hw6.py
@@ -1,3 +1,50 @@
-for i in range(21):
+n = int(input("請輸入一個整數："))
+for i in range(1, n + 1):
     if i % 3 == 0 or i % 7 == 0:
         print(i)
+
+import turtle as t
+
+t.speed(0)
+t.penup()
+for i in range(13):
+    t.forward(100)
+    t.stamp()
+    t.home()
+    t.right(30 * i)
+t.done()
+
+import turtle as t
+import time
+
+t.speed(0)
+for j in range(60):
+    t.right(6 * j)
+    t.forward(80)
+    time.sleep(1)
+    t.home()
+    t.clear()
+
+    print(f" " * (n - i) + "*")
+"""
+請輸入要印出的箭頭大小
+hint:
+可利用字串乘法
+>>>val="*" * 3
+>>>print(val)
+>>>***
+EX:
+請輸入要印出的箭頭大小:3
+  *
+ ***
+*****
+  *
+  *
+  *
+"""
+
+n = int(input("請輸入要印出的箭頭大小："))
+for i in range(1, n + 1):
+    print(f" " * (n - i) + "*" * (2 * i - 1))
+for i in range(1, n + 1):
+    print(f" " * (n - 1) + "*")
--- class5/tempCodeRunnerFile.py
+++ class5/tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
-for i in range(1, 10):          # 外層迴圈，表示乘數
-    for j in range(1, 10):      # 內層迴圈，被乘數
-        print(f"{i} x {j} = {i*j}", end="\t")
-    print()  # 換行
-
+n = int(input("請輸入要印出的箭頭大小："))
+for i in range(1, n + 1):
+    print(f" " * (n - i) + "*" * (2 * i - 1))
+for i in range(1, n + 1):
+    print(f" " * (n - 1) + "*")
