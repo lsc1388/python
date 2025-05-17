f37d48328698762c6994ec94e25fd73a7c217bd8
lsc1388 <scliao61413@gmail.com>
2025-05-17 10:32:41 +0800

    hw9.py

--- class9/hw9.py
+++ class9/hw9.py
@@ -32,3 +32,29 @@ while True:
         print("在小一點")
     else:
         print("在大一點")
+
+        juices_list = ["蘋果汁", "柳橙汁", "葡萄汁", "系統關閉"]
+
+while True:
+    print("1. 蘋果汁")
+    print("2. 柳橙汁")
+    print("3. 葡萄汁")
+    print("4. 系統關閉")
+
+    choice = input("請輸入編號：")
+
+    try:
+        choice = int(choice)  # 直接嘗試轉換成數字
+    except:
+        print("輸入錯誤查無此果汁，請重新輸入\n")
+        continue  # 失敗就請他重來
+
+    if choice < 1 or choice > len(juices_list):
+        print("輸入錯誤查無此果汁，請重新輸入\n")
+        continue  # 超出選項範圍也請他重來
+
+    if choice == 4:
+        print("~~系統關閉~~")
+        break
+
+    print(f"您點的商品是 {juices_list[choice-1]}\n")
